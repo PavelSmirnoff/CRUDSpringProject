f0715e64139529edbe8f922ebb5ece06f9cda9dc
Pavel Smirnov <smirnov@0x55.ru>
2021-02-16 19:31:31 +0300

    created web config

---
+++ src/main/java/dev/smirnov/crudspringproject/config/RootConfig.java
@@ -0,0 +1,11 @@
+package dev.smirnov.crudspringproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author pavelsmirnov
+ */
+@Configuration
+public class RootConfig implements WebMvcConfigurer {
+}
