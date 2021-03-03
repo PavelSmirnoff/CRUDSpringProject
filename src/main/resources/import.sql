7e9e281de7f108404899499075e79976742ee1a7
Pavel Smirnov <smirnov@0x55.ru>
2021-03-03 16:36:42 +0300

    Initial start user admin:admin

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO users(id, firstName, password) VALUES (1, 'Admin', '1');
+INSERT INTO users(id, firstName, password) VALUES (1, 'admin', 'admin');
 INSERT INTO role(id, name) VALUES (1, 'ROLE_ADMIN'), (2, 'ROLE_USER');
 INSERT INTO users_role(User_id, roles_id) VALUES (1, 1);
