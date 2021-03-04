353c39a56776ce0dc781795bae28e0c908691d69
Pavel Smirnov <smirnov@0x55.ru>
2021-03-04 20:29:23 +0300

    Add LAZY to Role set

--- src/main/java/dev/smirnov/crudspringproject/controller/AdminController.java
+++ src/main/java/dev/smirnov/crudspringproject/controller/AdminController.java
@@ -6,6 +6,7 @@ import dev.smirnov.crudspringproject.service.RoleService;
 import dev.smirnov.crudspringproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/dev/smirnov/crudspringproject/model/User.java
+++ src/main/java/dev/smirnov/crudspringproject/model/User.java
@@ -1,5 +1,7 @@
 package dev.smirnov.crudspringproject.model;
 
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -37,7 +39,9 @@ public class User implements UserDetails {
     @Transient
     private String passwordConfirm;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @Fetch(FetchMode.JOIN)
+    @ManyToMany(fetch = FetchType.LAZY)
+    //@ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles;
 
     public User() {}
